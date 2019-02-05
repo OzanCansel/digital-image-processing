b8e3c2949439661ff6b4e7c873b02c45ed1801e8
Ozan Cansel <ozanc@bites.local>
2019-02-05 08:40:10 +0300

    Information texts are added. Scaling functions refactored.

--- interpolation/main.cpp
+++ interpolation/main.cpp
@@ -31,86 +31,17 @@
 
 using namespace cv;
 
-auto targetWidth = 480;
-auto targetHeight = 480;
-
 /*
     It was benefitted from https://www.giassa.net/?page_id=207 while the algorithm is implementing.
 */
-Mat nearestNeightbourInterpolation(Mat input)
-{
-    Mat output = Mat::zeros(targetHeight , targetWidth , CV_8U);
-
-    auto xScale = static_cast<float>(targetWidth) / input.cols;
-    auto yScale = static_cast<float>(targetHeight) / input.rows;
-
-    for(auto x = 0; x < targetWidth; ++x)
-    {
-        for(auto y = 0; y < targetHeight; ++y)
-        {
-            auto fromX = static_cast<int>( std::round( (x + 1) / xScale));
-            auto fromY = static_cast<int>(std::round( (y + 1) / yScale));
-
-            output.at<uchar>(y , x) = input.at<uchar>(std::max(0 , fromY - 1) , std::max(0 , fromX - 1));
-        }
-    }
-
-    return output;
-}
+Mat nearestNeightbourInterpolation(Mat input , int targetWidth , int targetHeight);
 
 /*
     It was benefitted from https://stackoverflow.com/a/32128947 while the algorithm is implementing.
 */
-Mat bilinearInterpolation(Mat input)
-{
-    Mat output = Mat::zeros(targetHeight , targetWidth , CV_8U);
+Mat bilinearInterpolation(Mat input , int targetWidth , int targetHeight);
 
-    auto xScale = static_cast<float>(targetWidth) / input.cols;
-    auto yScale = static_cast<float>(targetHeight) / input.rows;
-
-    for(auto x = 0; x < targetWidth; ++x)
-    {
-        for(auto y = 0; y < targetHeight; ++y)
-        {
-            auto origX = (x / xScale) + 1;
-            auto origY = (y / yScale) + 1;
-            auto x1 = std::floor(origX);
-            auto x2 = std::ceil(origX);
-            auto y1 = std::floor(origY);
-            auto y2 = std::ceil(origY);
-
-            auto x1Idx = std::max(0 , static_cast<int>(x1) - 2);
-            auto x2Idx = std::max(0 , static_cast<int>(x2) - 2);
-            auto y1Idx = std::max(0 , static_cast<int>(y1) - 2);
-            auto y2Idx = std::max(0 , static_cast<int>(y2) - 2);
-
-
-            auto r1  = 0;
-            auto r2 = 0;
-
-            if(x1 == x2)
-            {
-                r1 = input.at<uchar>(y1Idx , x1Idx);
-                r2 = input.at<uchar>(y2Idx , x1Idx);
-            }
-            else
-            {
-                r1 = std::round(input.at<uchar>(y1Idx , x1Idx) + ((origX - x1) / 1.0) * (input.at<uchar>(y1Idx , x2Idx) - input.at<uchar>(y1Idx , x1Idx)));
-                r2 = std::round(input.at<uchar>(y2Idx , x1Idx) + ((origX - x1) / 1.0) * (input.at<uchar>(y2Idx , x2Idx) - input.at<uchar>(y2Idx , x1Idx)));
-            }
-            
-            auto P = 0;
-            if(r1 == r2)
-                P = r1;
-            else
-                P = std::max(.0f , std::min(255.0f , std::round((r2 + (origY - y2) / (y2 - y1) * (r1 - r2)))));
-
-            output.at<uchar>(y , x) = P;
-        }
-    }
-
-    return output;
-}
+Mat bicubicInterpolation(Mat input, int targetWidth, int targetHeight);
 
 int main(int argc , char** argv){
 
@@ -139,6 +70,10 @@ int main(int argc , char** argv){
         input = imread("interpolation.jpg");
     }
 
+	auto targetWidth = 0;
+	auto targetHeight = 0;
+
+
     if(cmdParser.has("width"))
         targetWidth = cmdParser.get<int>("width");
     if(cmdParser.has("height"))
@@ -153,15 +88,96 @@ int main(int argc , char** argv){
     //Change color format to grayscale
     cvtColor(input , input, COLOR_BGR2GRAY);
 
-    Mat nearestNeighbourOutput = nearestNeightbourInterpolation(input);
-    Mat bilinearInterpolationOutput = bilinearInterpolation(input);
+    Mat nearestNeighbourOutput = nearestNeightbourInterpolation(input , targetWidth , targetHeight);
+    Mat bilinearInterpolationOutput = bilinearInterpolation(input , targetWidth , targetHeight);
+	Mat bicubicInterpolationOutput = bicubicInterpolation(input, targetWidth, targetHeight);
 
-    namedWindow("Input Image", WINDOW_AUTOSIZE );
-    namedWindow("nearest neighbour interpolation" , WINDOW_AUTOSIZE);
-    namedWindow("bilinear interpolation" , WINDOW_AUTOSIZE);
-    imshow("Input Image", input);
-    imshow("nearest neighbour interpolation" , nearestNeighbourOutput);
-    imshow("bilinear interpolation" , bilinearInterpolationOutput);
+	auto inputInformationText = std::string("Size(w:") + std::to_string(input.cols) + std::string(" h:") + std::to_string(input.rows) + std::string(")");
+	auto sizeInformationText = std::string("Actual(w:") + std::to_string(input.cols) + std::string(" h:") + std::to_string(input.rows) + 
+		std::string(") Target(w:") + std::to_string(targetWidth) + std::string(" h:") + std::to_string(targetHeight) + std::string(")");
+
+    imshow(std::string("Input Image") = inputInformationText, input);
+    imshow(std::string("nearest neighbour interpolation") + sizeInformationText , nearestNeighbourOutput);
+    imshow(std::string("bilinear interpolation") + sizeInformationText , bilinearInterpolationOutput);
     waitKey(0);
     return 0;
+}
+
+Mat nearestNeightbourInterpolation(Mat input , int targetWidth , int targetHeight)
+{
+	Mat output = Mat::zeros(targetHeight, targetWidth, CV_8U);
+
+	auto xScale = static_cast<float>(targetWidth) / input.cols;
+	auto yScale = static_cast<float>(targetHeight) / input.rows;
+
+	for (auto x = 0; x < targetWidth; ++x)
+	{
+		for (auto y = 0; y < targetHeight; ++y)
+		{
+			auto fromX = static_cast<int>(std::round((x + 1) / xScale));
+			auto fromY = static_cast<int>(std::round((y + 1) / yScale));
+
+			output.at<uchar>(y, x) = input.at<uchar>(std::max(0, fromY - 1), std::max(0, fromX - 1));
+		}
+	}
+
+	return output;
+}
+
+Mat bilinearInterpolation(Mat input, int targetWidth, int targetHeight)
+{
+	Mat output = Mat::zeros(targetHeight, targetWidth, CV_8U);
+
+	auto xScale = static_cast<float>(targetWidth) / input.cols;
+	auto yScale = static_cast<float>(targetHeight) / input.rows;
+
+	for (auto x = 0; x < targetWidth; ++x)
+	{
+		for (auto y = 0; y < targetHeight; ++y)
+		{
+			auto origX = (x / xScale) + 1;
+			auto origY = (y / yScale) + 1;
+			auto x1 = std::floor(origX);
+			auto x2 = std::ceil(origX);
+			auto y1 = std::floor(origY);
+			auto y2 = std::ceil(origY);
+
+			auto x1Idx = std::max(0, static_cast<int>(x1) - 2);
+			auto x2Idx = std::max(0, static_cast<int>(x2) - 2);
+			auto y1Idx = std::max(0, static_cast<int>(y1) - 2);
+			auto y2Idx = std::max(0, static_cast<int>(y2) - 2);
+
+
+			auto r1 = 0;
+			auto r2 = 0;
+
+			if (x1 == x2)
+			{
+				r1 = input.at<uchar>(y1Idx, x1Idx);
+				r2 = input.at<uchar>(y2Idx, x1Idx);
+			}
+			else
+			{
+				r1 = std::round(input.at<uchar>(y1Idx, x1Idx) + ((origX - x1) / 1.0) * (input.at<uchar>(y1Idx, x2Idx) - input.at<uchar>(y1Idx, x1Idx)));
+				r2 = std::round(input.at<uchar>(y2Idx, x1Idx) + ((origX - x1) / 1.0) * (input.at<uchar>(y2Idx, x2Idx) - input.at<uchar>(y2Idx, x1Idx)));
+			}
+
+			auto P = 0;
+			if (r1 == r2)
+				P = r1;
+			else
+				P = std::max(.0f, std::min(255.0f, std::round((r2 + (origY - y2) / (y2 - y1) * (r1 - r2)))));
+
+			output.at<uchar>(y, x) = P;
+		}
+	}
+
+	return output;
+}
+
+Mat bicubicInterpolation(Mat input, int targetWidth, int targetHeight)
+{
+	Mat output;
+
+	return output;
 }
