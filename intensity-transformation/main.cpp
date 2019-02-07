1fdcb1be12bbf0bcfe631b79701119f1d9161381
Ozan Cansel <ozanc@bites.local>
2019-02-07 16:37:16 +0300

    intensity slicing is implemented.

--- intensity-transformation/main.cpp
+++ intensity-transformation/main.cpp
@@ -36,14 +36,19 @@ Mat powerTransformation(Mat input, double gamma);
 //Does log transformation, returns the output, chapter 3.2.2
 Mat logTransformation(Mat input , double gamma);
 //Contrast strecthing algorithm 3.2.4
+//It is benefitted from http://what-when-how.com/embedded-image-processing-on-the-tms320c6000-dsp/contrast-stretching-image-processing/
 Mat contrastStretching(Mat input);
+//Intensity Slicing, Figure 3.11(a) implementation
+Mat intensitySlicing(Mat input , int from , int to);
 
 int main(int argc, char** argv) {
         const String keys = 
 	"{help h usage ?    || The program does intensity transformations such as log and power transformations.}"
     "{input             | intensity-transformation-dark.jpg | power and log transforming image}"
+	"{gamma             | 1.15								| gamma value for power transformation}"
 	"{input2            | contrast-stretching.jpg           | power and log transforming image}"
-	"{gamma             | 1.15								| gam|ma value for power transformation}"
+	"{slicingFrom       | 50								| A value of interval [A , B]}"
+	"{slicingTo         | 81								| B value of interval [A , B]}"
     ;
 
     CommandLineParser cmdParser(argc , argv, keys);
@@ -59,6 +64,8 @@ int main(int argc, char** argv) {
     input = imread(cmdParser.get<cv::String>("input").c_str());
 	input2 = imread(cmdParser.get<cv::String>("input2").c_str());
     auto gamma = cmdParser.get<double>("gamma");
+	auto slicingFrom = cmdParser.get<int>("slicingFrom");
+	auto slicingTo = cmdParser.get<int>("slicingTo");
 
     if ( !input.data )
     {
@@ -71,12 +78,14 @@ int main(int argc, char** argv) {
     auto powerTransformedOutput = powerTransformation(input , gamma);
     auto logTransformedOutput = logTransformation(input , gamma);
 	auto contrastStrecthedOutput = contrastStretching(input2);
+	auto intensitySlicedOutput = intensitySlicing(input2, slicingFrom, slicingTo);
 
     imshow("input1" , input);
     imshow((std::string("Power Transformation - Gamma ") + std::to_string(gamma)).c_str() , powerTransformedOutput);
     imshow("Log Transformation" , logTransformedOutput);
 	imshow("input2", input2);
 	imshow("Contrast Streching", contrastStrecthedOutput);
+	imshow("Intensity Slicing", intensitySlicedOutput);
 
     waitKey(0);
 
@@ -169,6 +178,46 @@ Mat contrastStretching(Mat input)
 		{
 			auto p = input.at<uchar>(y, x);
 			result.at<uchar>(y , x) = p = std::round(((p - rMin) / R) * 255.0);
+		}
+	}
+
+	return result;
+}
+
+Mat intensitySlicing(Mat input , int from, int to)
+{
+	Mat result = Mat::zeros(input.rows, input.cols, CV_8U);
+
+	auto rMin = 255;
+	auto rMax = 0;
+
+	//Find min and max values in the image
+	for (auto y = 0; y < input.rows; ++y)
+	{
+		for (auto x = 0; x < input.cols; ++x)
+		{
+			auto current = input.at<uchar>(y, x);
+
+			if (current < rMin)
+				rMin = current;
+
+			if (current > rMax)
+				rMax = current;
+		}
+	}
+
+	auto R = static_cast<double>(rMax - rMin);
+
+	//Strecth contrast between rMin and rMax to 0 and 255
+	for (auto y = 0; y < input.rows; ++y)
+	{
+		for (auto x = 0; x < input.cols; ++x)
+		{
+			auto p = input.at<uchar>(y, x);
+			if (p >= from && p <= to)
+				result.at<uchar>(y, x) = p = std::round(((p - rMin) / R) * 255.0);
+			else
+				result.at<uchar>(y, x) = rMin;
 		}
 	}
 
